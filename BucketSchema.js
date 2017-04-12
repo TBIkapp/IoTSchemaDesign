b2ac4a181f2ce58de5b02dde7bc4993005a5a362
Tim Biedenkapp <github@tim-biedenkapp.de>
2017-04-12 09:21:54 +0200

    document examples

---
+++ BucketSchema.js
@@ -0,0 +1,20 @@
+{
+  bucket_id : 125378,
+  start_date : ISODate(“2017-02-25T17:00:00.000Z”),
+  end_date : ISODate(“2017-02-25T18:00:00.000Z”),
+  txCount : 100,
+  measurements : [
+	{
+        	date : ISODate(“2017-02-25T17:13:00.000Z”),
+        	sensor_id : 520124,
+        	temperature : 9,
+        	humidity : 60
+	},
+	{
+  	  	date : ISODate(“2017-02-25T17:13:05.000Z”),
+        	sensor_id : 520124,
+        	temperature : 15,
+        	humidity : 55
+	}, …
+  ]
+}
---
+++ SimpleSchema.js
@@ -0,0 +1,21 @@
+{
+  sensor_id : 520124,
+  sensor_name : ‘temperature_frankfurt_north’,
+  city : ‘Frankfurt,
+  location : {
+        	type : ‘Point’,
+        	coordinates : [50.328315, 8.749493]
+  },    	
+  Measurements: [
+{
+name : ‘temperature’,
+measurement : ‘celcius’
+        	},
+        	{       	name : ‘humidity’,
+                    	measurement : ‘percent’
+        	}
+  ],
+  install_date : ISODate(“2017-01-15T09:17:18.000Z”),
+  HW_Version : 1.3,
+  SW_Version : 2.4
+}
