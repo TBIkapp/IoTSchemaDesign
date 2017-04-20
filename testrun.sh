d687c987f485fa0f91efe92202e7cbaa8a0a0cab
Tim Biedenkapp <github@tim-biedenkapp.de>
2017-04-20 11:10:28 +0200

    added db.stats()

--- testrun.sh
+++ testrun.sh
@@ -5,23 +5,26 @@ mkdir -p logs/
 echo "++++ 1min test run ++++"
 
 echo "++ MMAPV1 Test ++"
-mkdir -p data/mmapv1
-./mongod --storageEngine mmapv1 --dbpath data/mmapv1 --fork --logpath logs/mmapv1.log
+mkdir -p data/mmapv1_1min
+./mongod --storageEngine mmapv1 --dbpath data/mmapv1_1min --fork --logpath logs/mmapv1.log
 ./mongo IoTTest IoTGenerator.js
+./mongo IoTTest --eval "db.stats()"
 killall mongod
 rm -r data/*
 
 echo "++ WiredTiger Test ++"
-mkdir -p data/wt
-./mongod --dbpath data/wt --fork --logpath logs/wt.log
+mkdir -p data/wt_1min
+./mongod --dbpath data/wt_1min --fork --logpath logs/wt.log
 ./mongo IoTTest IoTGenerator.js
+./mongo IoTTest --eval "db.stats()"
 killall mongod
 rm -r data/*
 
 echo "++ inMemory Test ++"
-mkdir -p data/inMemory
-./mongod --storageEngine inMemory --dbpath data/inMemory --fork --logpath logs/inMemory.log
+mkdir -p data/inMemory_1min
+./mongod --storageEngine inMemory --dbpath data/inMemory_1min --fork --logpath logs/inMemory.log
 ./mongo IoTTest IoTGenerator.js
+./mongo IoTTest --eval "db.stats()"
 killall mongod
 rm -r data/*
 
@@ -29,23 +32,26 @@ rm -r data/*
 echo "++++ 10min test run ++++"
 
 echo "++ MMAPV1 Test ++"
-mkdir -p data/mmapv1
-./mongod --storageEngine mmapv1 --dbpath data/mmapv1 --fork --logpath logs/mmapv1.log
+mkdir -p data/mmapv1_10min
+./mongod --storageEngine mmapv1 --dbpath data/mmapv1_10min --fork --logpath logs/mmapv1.log
 ./mongo IoTTest IoTGenerator10min.js
+./mongo IoTTest --eval "db.stats()"
 killall mongod
 rm -r data/*
 
 echo "++ WiredTiger Test ++"
-mkdir -p data/wt
-./mongod --dbpath data/wt --fork --logpath logs/wt.log
+mkdir -p data/wt_10min
+./mongod --dbpath data/wt_10min --fork --logpath logs/wt.log
 ./mongo IoTTest IoTGenerator10min.js
+./mongo IoTTest --eval "db.stats()"
 killall mongod
 rm -r data/*
 
 echo "++ inMemory Test ++"
-mkdir -p data/inMemory
-./mongod --storageEngine inMemory --dbpath data/inMemory --fork --logpath logs/inMemory.log
+mkdir -p data/inMemory_10min
+./mongod --storageEngine inMemory --dbpath data/inMemory_10min --fork --logpath logs/inMemory.log
 ./mongo IoTTest IoTGenerator10min.js
+./mongo IoTTest --eval "db.stats()"
 killall mongod
 rm -r data/*
 
@@ -53,23 +59,26 @@ rm -r data/*
 echo "++++ 1hour test run ++++"
 
 echo "++ MMAPV1 Test ++"
-mkdir -p data/mmapv1
-./mongod --storageEngine mmapv1 --dbpath data/mmapv1 --fork --logpath logs/mmapv1.log
+mkdir -p data/mmapv1_1hr
+./mongod --storageEngine mmapv1 --dbpath data/mmapv1_1hr --fork --logpath logs/mmapv1.log
 ./mongo IoTTest IoTGenerator1hour.js
+./mongo IoTTest --eval "db.stats()"
 killall mongod
 rm -r data/*
 
 echo "++ WiredTiger Test ++"
-mkdir -p data/wt
-./mongod --dbpath data/wt --fork --logpath logs/wt.log
+mkdir -p data/wt_1hr
+./mongod --dbpath data/wt_1hr --fork --logpath logs/wt.log
 ./mongo IoTTest IoTGenerator1hour.js
+./mongo IoTTest --eval "db.stats()"
 killall mongod
 rm -r data/*
 
 echo "++ inMemory Test ++"
-mkdir -p data/inMemory
-./mongod --storageEngine inMemory --dbpath data/inMemory --fork --logpath logs/inMemory.log
+mkdir -p data/inMemory_1hr
+./mongod --storageEngine inMemory --dbpath data/inMemory_1hr --fork --logpath logs/inMemory.log
 ./mongo IoTTest IoTGenerator1hour.js
+./mongo IoTTest --eval "db.stats()"
 killall mongod
 rm -r data/*
 
